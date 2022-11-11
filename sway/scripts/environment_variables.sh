e2c5a98085562f53942f5c2d599e53c55585547e
Justin <justincaveda11031@gmail.com>
2022-11-10 22:42:05 -0600

    auto-update

--- sway/scripts/environment_variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export XDG_CURRENT_DESKTOP=sway
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx
-export MANGOHUD=1
-export KITTY_ENABLE_WAYLAND=1
-export WLR_NO_HARDWARE_CURSORS=1
