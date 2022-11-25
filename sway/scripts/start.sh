81fd41e446782174f4b4d40e917d0f5115d9e46c
zoro11031 <justincaveda11031@gmail.com>
2022-11-25 16:01:50 -0600

    auto-update

--- sway/scripts/start.sh
+++ sway/scripts/start.sh
@@ -6,7 +6,7 @@ swaymsg 'workspace $ws3; exec thunar'
 
 swaymsg 'exec flatpak run com.discordapp.DiscordCanary && move container to workspace $ws2'
 
-swaymsg 'exec flatpak run com.valvesoftware.Steam --silent && move container to workspace $ws5'
+swaymsg 'exec steam-runtime --silent && move container to workspace $ws5'
 
 swaymsg 'workspace $ws1; exec firefox && move container to workspace $ws1'
 
